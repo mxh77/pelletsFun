185fa9ce3baddd45b321ecba9935aaf120cb124f
mxh77 <maxime.heron@gmail.com>
2025-11-02 08:08:23 +0100

    Fix basename for production deployment

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,16 @@ import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import { Tabs, Tab } from 'react-bootstrap';
 import DeliveryForm from './components/DeliveryForm';
 import DeliveryList from './components/DeliveryList';
-import EditDeliveryForm from './components/EditDeliveryForm'; // Importer le formulaire de modification des livraisons
+import EditDeliveryForm from './components/EditDeliveryForm';
 import RechargeForm from './components/RechargeForm';
 import RechargeList from './components/RechargeList';
-import EditRechargeForm from './components/EditRechargeForm'; // Importer le formulaire de modification des chargements
+import EditRechargeForm from './components/EditRechargeForm';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <Router basename="/pelletsFun">
+    <Router basename="/">
       <div className="App container">
         <header className="App-header">
           <h1 className="my-4">Pellets Tracker</h1>
@@ -23,9 +23,9 @@ function App() {
         <Routes>
           <Route path="/" element={<TabsComponent />} />
           <Route path="/add-delivery" element={<DeliveryForm />} />
-          <Route path="/edit-delivery/:id" element={<EditDeliveryForm />} /> {/* Ajouter la route pour EditDeliveryForm */}
+          <Route path="/edit-delivery/:id" element={<EditDeliveryForm />} />
           <Route path="/add-recharge" element={<RechargeForm />} />
-          <Route path="/edit-recharge/:id" element={<EditRechargeForm />} /> {/* Ajouter la route pour EditRechargeForm */}
+          <Route path="/edit-recharge/:id" element={<EditRechargeForm />} />
         </Routes>
       </div>
     </Router>
