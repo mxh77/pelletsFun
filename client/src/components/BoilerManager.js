882d0ec30a91fa96ec189984797a9889701f609d
mxh77 <maxime.heron@gmail.com>
2025-11-08 09:23:42 +0100

    refonte

--- client/src/components/BoilerManager.js
+++ client/src/components/BoilerManager.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './BoilerManagerRestructured.css';
+import './BoilerManager.css';
 
 const BoilerManager = () => {
   // États principaux
